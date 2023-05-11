ce7595e7ca420a5e8a1094ec1635813c041c1986
root <root@ip-172-31-28-33.us-east-2.compute.internal>
2023-05-11 08:36:33 +0000

    key name changed

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "Intellipaat"
+  key_name      = "terraform"
   user_data     = <<-EOF
                     #!/bin/bash
                     sudo apt update -y
